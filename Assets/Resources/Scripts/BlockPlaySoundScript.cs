e19a576b20031e4e51f524af9cd7d5cbed705038
Connie <cleung334@gmail.com>
2014-05-17 19:40:12 +0800

    initial commit

---
+++ Assets/Resources/Scripts/BlockPlaySoundScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockPlaySoundScript : MonoBehaviour {
+	
+	bool m_hasBeenHit = false;
+
+	void OnCollisionEnter(Collision c) {
+		if (m_hasBeenHit) return;
+		m_hasBeenHit = true;
+		// play a sound when collision occurs
+		audio.Play ();
+	}
+	
+	void Update() {
+	}
+	
+	void OnCollisionExit(Collision collisionInfo) {
+		m_hasBeenHit = false;
+	}
+
+
+}
---
+++ Assets/Resources/Scripts/BlockScoreLogic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class BlockScoreLogic : MonoBehaviour {
+	
+	//AudioClip sound;
+
+	bool m_hasBeenHit = false;
+	public void ResetScore() {
+		m_hasBeenHit = false;
+	}
+
+	void OnCollisionEnter(Collision c) {
+		if (m_hasBeenHit) return;
+		m_hasBeenHit = true;
+		// play a sound when collision occurs
+		//audio.Play ();
+		GameObject.Find("Score").GetComponent<ScoreDisplay>().m_score++;
+	}
+
+	void Update() {
+		// clean up after ourselves...
+		if (transform.position.z < -20.0f) Destroy(gameObject);
+	}
+}
